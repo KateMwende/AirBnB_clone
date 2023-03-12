b6077af7af26a00d4575c596a38f0e53dabc8799
Katemwende <kmwende419@gmail.com>
2023-03-12 20:50:33 +0000

    try

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,12 +4,6 @@ to a JSON file and deserializes JSON file to instances
 """
 
 import json
-from models.user import User
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 from os import path
 
 
