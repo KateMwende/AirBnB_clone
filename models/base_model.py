aef95f624e2c878db19206488312bc192beae4b6
Katemwende <kmwende419@gmail.com>
2023-03-12 12:10:31 +0000

    correction

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """Return strings of class name, id and dictionary of attributes"""
@@ -41,7 +41,7 @@ class BaseModel:
         """update updated_at with the current datetime"""
 
         self.updated_at = datetime.now()i
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """returns all keys/values of __dict__ of the instance"""
